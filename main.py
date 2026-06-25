0fa1ace4d7155d78023412907085630df8102682
rishurajgit <rajrishu.cs@gmail.com>
2026-06-25 18:58:47 +0530

    Query Router API working fine

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from fastapi import FastAPI
 from health.router import router as health_router
 from smart_text_toolkit.router import router as smart_text_router
 from resume_parser.router import router as resume_router
+from query_router_api.router import router as query_router
+
 
 app = FastAPI()
 
 app.include_router(health_router)
 app.include_router(smart_text_router)
 app.include_router(resume_router)
+app.include_router(query_router)
