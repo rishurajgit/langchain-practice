559acbd69fdeabd642600294733cebd1e7e6c33f
rishurajgit <rajrishu.cs@gmail.com>
2026-06-25 15:35:24 +0530

    Fix Feature

--- smart_text_toolkit/schema.py
+++ smart_text_toolkit/schema.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from enum import Enum
 
-class ActionType(str, Enum):
+class TransformAction(str, Enum):
     summarize = "summarize"
     translate = "translate"
     tone_shift = "tone_shift"
@@ -9,7 +9,7 @@ class ActionType(str, Enum):
     
 class TransformRequest(BaseModel):
     text: str
-    action: str
+    action: TransformAction
     
 class TransformResponse(BaseModel):
     result: str
