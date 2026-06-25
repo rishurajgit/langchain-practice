81f640e3a408f84450a8e64562c68b3c3cccb399
rishurajgit <rajrishu.cs@gmail.com>
2026-06-25 15:31:47 +0530

    Implemented Restriction for ActionType using Enum

--- smart_text_toolkit/schema.py
+++ smart_text_toolkit/schema.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
+from enum import Enum
 
+class ActionType(str, Enum):
+    summarize = "summarize"
+    translate = "translate"
+    tone_shift = "tone_shift"
+    
+    
 class TransformRequest(BaseModel):
     text: str
     action: str
