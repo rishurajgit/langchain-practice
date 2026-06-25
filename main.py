84c6a506252ed9d49f61a200c1e4885a8dfdad1d
rishurajgit <rajrishu.cs@gmail.com>
2026-06-25 18:22:38 +0530

    Resume Parser working fine

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from health.router import router as health_router
 from smart_text_toolkit.router import router as smart_text_router
+from resume_parser.router import router as resume_router
 
 app = FastAPI()
 
 app.include_router(health_router)
 app.include_router(smart_text_router)
+app.include_router(resume_router)
