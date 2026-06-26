748a2eaf1f268867ba0fd337c364d4cbd7a0744e
rishurajgit <rajrishu.cs@gmail.com>
2026-06-26 13:40:42 +0530

    Parallel Content Generation API Working Fine

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from health.router import router as health_router
 from smart_text_toolkit.router import router as smart_text_router
 from resume_parser.router import router as resume_router
 from query_router_api.router import router as query_router
-
+from parallel_content_generator_api.router import router as parallel_router
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ app.include_router(health_router)
 app.include_router(smart_text_router)
 app.include_router(resume_router)
 app.include_router(query_router)
+app.include_router(parallel_router)
